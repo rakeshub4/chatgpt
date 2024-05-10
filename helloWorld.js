12e5d193cced40cd282139feb05d812c515c0369
rakesh <rakeshub4@gmail.com>
2024-05-10 21:52:41 +0530

    Update helloWorld.js

---
+++ helloWorld.js
@@ -0,0 +1,12 @@
+/**
+ * Function: helloWorld
+ * Description: Prints "Hello, World!" to the console.
+ * @returns {string} - The "Hello, World!" message.
+ */
+function helloWorld() {
+    console.log("Hello, World!");
+    return "Hello, World!";
+}
+
+// Test case
+console.log(helloWorld()); // Output: Hello, World!
